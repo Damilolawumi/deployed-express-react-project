cbff4d6510b6c124a04adae564ec94337f2484d0
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-31 12:40:33 +0100

    added a few changes

--- Index.js
+++ Index.js
@@ -10,14 +10,17 @@ const friends = [
     { id: 1, name: 'Shaun' },
     { id: 2, name: 'Megan' },
     { id: 3, name: 'Pere' },
-]
+];
 
-
-
+app.use(express.static(__dirname + '/client/build'))
 app.use(express.json());
 app.use(cors());
 
-app.get('api/friends', (req, res, next) => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+});
+
+app.get('api/friends', (req, res) => {
     res.json(friends);
 })
 
