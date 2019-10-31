083ae5a75cb47b88b5dbb44f82fdd873d9625487
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-31 10:34:29 +0100

    added a few dependencies

--- Index.js
+++ Index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 const app = express();
 const friends = [
     { id: 1, name: 'Shaun' },
